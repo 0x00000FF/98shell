633f59adbc145a9aa0040e745b1863158ce88bf2
P.Knowledge <doctor@kangnang.net>
2019-01-10 21:03:54 +0900

    Initial Project Files

---
+++ src/98shell/ShellMain.cpp
@@ -0,0 +1,7 @@
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE hPrevInstance, LPSTR argumenets, int nCmdShow)
+{
+	MessageBox(NULL, "HELLO", "HELLO", MB_ICONINFORMATION);
+	return 0;
+}
